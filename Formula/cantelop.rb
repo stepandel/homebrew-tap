ba661c25a1a27ff843548fc4c6142f7de1439f0d
stepandel <stepandel@gmail.com>
2026-08-17 17:18:41 -0700

    Add Cantelop 0.1.0-rc.11

--- Formula/cantelop.rb
+++ Formula/cantelop.rb
@@ -4,29 +4,29 @@
 class Cantelop < Formula
   desc "Deploy complete API and harness releases to Cantelop"
   homepage "https://cantelop.com"
-  version "0.1.0-rc.5"
+  version "0.1.0-rc.11"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/stepandel/homebrew-tap/releases/download/cantelop-v0.1.0-rc.5/cantelop_0.1.0-rc.5_darwin_amd64.tar.gz"
-      sha256 "cf7b6b4f487c04fba4af29698f72c2f828440db8f7e3945dd9d518ba53ed64c9"
+      url "https://github.com/stepandel/homebrew-tap/releases/download/cantelop-v0.1.0-rc.11/cantelop_0.1.0-rc.11_darwin_amd64.tar.gz"
+      sha256 "09bb28a1382fb27779084abf60c3f9b21f87d5f0fc7787f7992cd219962befce"
     end
 
     if Hardware::CPU.arm?
-      url "https://github.com/stepandel/homebrew-tap/releases/download/cantelop-v0.1.0-rc.5/cantelop_0.1.0-rc.5_darwin_arm64.tar.gz"
-      sha256 "2af7f8937c78a16b0846f17ca1b5341ff35550aa8f23cb1fbe09ab10d65ae178"
+      url "https://github.com/stepandel/homebrew-tap/releases/download/cantelop-v0.1.0-rc.11/cantelop_0.1.0-rc.11_darwin_arm64.tar.gz"
+      sha256 "4c805416cc39ac5f3fbcbb9832ce640e5c8446a474044e0eb5e5a9f0795571f5"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/stepandel/homebrew-tap/releases/download/cantelop-v0.1.0-rc.5/cantelop_0.1.0-rc.5_linux_amd64.tar.gz"
-      sha256 "56ecb82702238d07a52ccf4c652884f50c918af6e25bce0b44387e15313cdb8d"
+      url "https://github.com/stepandel/homebrew-tap/releases/download/cantelop-v0.1.0-rc.11/cantelop_0.1.0-rc.11_linux_amd64.tar.gz"
+      sha256 "fe6470ae6d926c5651a3854f6f27b53c6d38efe358bcc1ad41b83f05e37ddb4a"
     end
 
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/stepandel/homebrew-tap/releases/download/cantelop-v0.1.0-rc.5/cantelop_0.1.0-rc.5_linux_arm64.tar.gz"
-      sha256 "32aa5b696168de75208ef08eea9ce768d04acd9a7f9de366f77fd865d4e89b12"
+      url "https://github.com/stepandel/homebrew-tap/releases/download/cantelop-v0.1.0-rc.11/cantelop_0.1.0-rc.11_linux_arm64.tar.gz"
+      sha256 "7776a0f4720a58e2c40d03c633525a0ee6cd837d4de4be4b49c4603082a6aa41"
     end
   end
 
